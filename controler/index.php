625c1fd319552001877d0d069544d6a334d419d2
arthur <arthur32810@gmail.com>
2021-08-19 18:35:18 +0200

    filter input

--- controler/index.php
+++ controler/index.php
@@ -2,14 +2,17 @@
 
 require '../vendor/autoload.php';
 
-$action = isset($_GET['action']) ? $_GET['action'] : null;
+$action = filter_input(INPUT_GET, 'action');
 
-if (isset($action)) {
-    if ($actions == 'posts') { //Affiche tout les posts
+if ($action) {
+    if ($action == 'posts') { //Affiche tout les posts
         //Récupére tout les posts
         //Affiche les posts
         echo 'posts';
         var_dump(__DIR__);
+    } else {
+        //Erreur 404
+        echo '404';
     }
 } else {
     require '../view/frontend/index.php';
